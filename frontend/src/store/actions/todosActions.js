eab0cc02b5ce699f7b3f9a58204e9a00e5158073
Your Name <yhyrvbd@gmail.com>
2023-07-18 12:59:24 -0700

    Update packages except react in 2023

--- frontend/src/store/actions/todosActions.js
+++ frontend/src/store/actions/todosActions.js
@@ -1,51 +1,51 @@
-import { apiCall } from '../../services/api';
-import { addError } from './errorsActions';
+import { apiCall } from "../../services/api";
+import { addError } from "./errorsActions";
 
-import { ADD_TODO, REMOVE_TODO, GET_TODOS } from './types';
+import { ADD_TODO, REMOVE_TODO, GET_TODOS } from "./types";
 
 export const loadTodos = (todos) => ({
-	type: GET_TODOS,
-	todos
+  type: GET_TODOS,
+  todos,
 });
 
-export const remove = (id) => ({
-	type: REMOVE_TODO,
-	id
+export const removeTodoTask = (id) => ({
+  type: REMOVE_TODO,
+  id,
 });
 
 export const addNewTodo = (todo) => ({
-	type: ADD_TODO,
-	todo
+  type: ADD_TODO,
+  todo,
 });
 
 export const fetchTodos = () => {
-	return (dispatch) => {
-		return apiCall('get', '/api/todos')
-			.then((res) => {
-				dispatch(loadTodos(res));
-			})
-			.catch((err) => {
-				dispatch(addError(err.message));
-			});
-	};
+  return (dispatch) => {
+    return apiCall("get", "/api/todos")
+      .then((res) => {
+        dispatch(loadTodos(res));
+      })
+      .catch((err) => {
+        dispatch(addError(err.message));
+      });
+  };
 };
 
 export const addTodo = (task) => (dispatch) => {
-	return apiCall('post', '/api/todos', { task })
-		.then((res) => {
-			console.log(`inside todosActions, addTodo res: `);
-			console.log(res);
-			dispatch(addNewTodo(res));
-		})
-		.catch((err) => {
-			dispatch(addError(err.message));
-		});
+  return apiCall("post", "/api/todos", { task })
+    .then((res) => {
+      console.log(`inside todosActions, addTodo res: `);
+      console.log(res);
+      dispatch(addNewTodo(res));
+    })
+    .catch((err) => {
+      dispatch(addError(err.message));
+    });
 };
 
 export const removeTodo = (id) => {
-	return (dispatch) => {
-		return apiCall('delete', `/api/todos/${id}`)
-			.then(() => dispatch(remove(id)))
-			.catch((err) => dispatch(addError(err.message)));
-	};
+  return (dispatch) => {
+    return apiCall("delete", `/api/todos/${id}`)
+      .then(() => dispatch(removeTodoTask(id)))
+      .catch((err) => dispatch(addError(err.message)));
+  };
 };
